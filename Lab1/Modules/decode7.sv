3c828afc0337bba893dab7f2214b7c9103c53e79
teylorwong <teylorwong88@gmail.com>
2025-01-17 09:47:49 -0800

    Finished week2 for lab1

--- Lab1/Modules/decode7.sv
+++ Lab1/Modules/decode7.sv
@@ -7,21 +7,22 @@ module decode7 (
 
     always_comb
         case (num)
-            4'h0 : leds = 8'h7E;   // 0 on 7-Segment Display
-            4'h1 : leds = 8'h30;   // 1 on 7-Segment Display
-            4'h2 : leds = 8'h6D;   // 2 on 7-Segment Display
-            4'h3 : leds = 8'h79;   // 3 on 7-Segment Display
-            4'h4 : leds = 8'h33;   // 4 on 7-Segment Display
-            4'h5 : leds = 8'h5B;   // 5 on 7-Segment Display
-            4'h6 : leds = 8'h5F;   // 6 on 7-Segment Display
-            4'h7 : leds = 8'h70;   // 7 on 7-Segment Display
-            4'h8 : leds = 8'h7F;   // 8 on 7-Segment Display
-            4'h9 : leds = 8'h7B;   // 9 on 7-Segment Display
-            4'hA : leds = 8'h77;   // A on 7-Segment Display
-            4'hB : leds = 8'h1F;   // b on 7-Segment Display
-            4'hC : leds = 8'h4E;   // C on 7-Segment Display
-            4'hD : leds = 8'h3D;   // d on 7-Segment Display
-            4'hE : leds = 8'h4F;   // E on 7-Segment Display
-            4'hF : leds = 8'h47;   // F on 7-Segment Display
+	    4'h0: leds = 8'b00111111; // 0
+            4'h1: leds = 8'b00000110; // 1
+            4'h2: leds = 8'b01011011; // 2
+            4'h3: leds = 8'b01001111; // 3
+            4'h4: leds = 8'b01100110; // 4
+            4'h5: leds = 8'b01101101; // 5
+            4'h6: leds = 8'b01111101; // 6
+            4'h7: leds = 8'b00000111; // 7
+            4'h8: leds = 8'b01111111; // 8
+            4'h9: leds = 8'b01101111; // 9
+            4'hA: leds = 8'b01110111; // A
+            4'hB: leds = 8'b01111100; // b
+            4'hC: leds = 8'b00111001; // C
+            4'hD: leds = 8'b01011110; // d
+            4'hE: leds = 8'b01111001; // E
+            4'hF: leds = 8'b01110001; // F
+            default: leds = 8'b00000000; // Default case
         endcase
 endmodule
