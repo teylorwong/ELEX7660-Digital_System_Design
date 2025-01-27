f06913e8b6b1a1b56c9f555abeca59e78d3ea5f4
teylorwong <teylorwong88@gmail.com>
2025-01-27 14:56:01 -0800

    Updated encoder.sv to be cleaner

--- lab3/Modules/encoder.sv
+++ lab3/Modules/encoder.sv
@@ -26,18 +26,18 @@ module encoder (
 
         // Transition detection
         if ((prev_a != a) || (prev_b != b)) begin
-            if ((prev_a == 0 && prev_b == 0 && a == 1 && b == 0) || // 00 -> 10
-                (prev_a == 1 && prev_b == 0 && a == 1 && b == 1) || // 10 -> 11
-                (prev_a == 1 && prev_b == 1 && a == 0 && b == 1) || // 11 -> 01
-                (prev_a == 0 && prev_b == 1 && a == 0 && b == 0))   // 01 -> 00
+            if ((!prev_a && !prev_b && a && !b) || // 00 -> 10
+                (prev_a && !prev_b  && a && b) || // 10 -> 11
+                (prev_a && prev_b && !a && b) || // 11 -> 01
+                (!prev_a && prev_b && !a && !b))   // 01 -> 00
             begin
                 cw <= 1'b1;  // Generate CW pulse
                 current_state <= CW_DETECT;  // Update to CW_DETECT state
             end
-            else if ((prev_a == 0 && prev_b == 0 && a == 0 && b == 1) || // 00 -> 01
-                     (prev_a == 0 && prev_b == 1 && a == 1 && b == 1) || // 01 -> 11
-                     (prev_a == 1 && prev_b == 1 && a == 1 && b == 0) || // 11 -> 10
-                     (prev_a == 1 && prev_b == 0 && a == 0 && b == 0))   // 10 -> 00
+            else if ((!prev_a && !prev_b && !a && b) || // 00 -> 01
+                     (!prev_a && prev_b && a && b) || // 01 -> 11
+                     (prev_a && prev_b && a && !b) || // 11 -> 10
+                     (prev_a && !prev_b && !a && !b))   // 10 -> 00
             begin
                 ccw <= 1'b1;  // Generate CCW pulse
                 current_state <= CCW_DETECT;  // Update to CCW_DETECT state
